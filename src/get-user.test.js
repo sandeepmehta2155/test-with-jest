8dba3fe34d07a96df8cb91ee76997e62d49d6dd3
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2021-07-28 13:17:27 +0530

    test added

---
+++ src/get-user.test.js
@@ -0,0 +1,14 @@
+import getUser from "./get-user";
+import * as axios from "axios";
+
+jest.mock("axios");
+
+describe("get user service", () => {
+  test("should return user when API call is successful", async () => {
+    expect(true).toBe(false);
+  });
+
+  test("should return errorMessage when API is call fails", async () => {
+    expect(true).toBe(false);
+  });
+});
